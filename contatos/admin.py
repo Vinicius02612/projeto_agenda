9f247e64fc95c74e6243e7503b163a1957253dc2
Vinicius02612 <viniciusnunes02612@gmail.com>
2023-02-19 22:21:02 -0300

    Criando paginação

--- contatos/admin.py
+++ contatos/admin.py
@@ -7,13 +7,16 @@ from .models import Categoria, Contato
 class ContatoAdmin(admin.ModelAdmin):
 
     # mostra todos os campos da tabela contato
-    list_display = ('id','nome', 'sobrenome','telefone', 'email', 'descricao','categoria')
+    list_display = ('id','nome', 'sobrenome','telefone', 'email', 'descricao','categoria','mostrar')
 
     # torna apenas os campo id e nome clicaveis(links)
     list_display_links = ('id', 'nome')
 
     #cria campo de busca
     search_fields = ('id', 'nome', 'sobrenome', 'email')
+
+    #campos que podem ser editaveis
+    list_editable = ('telefone', 'mostrar')
 
 admin.site.register(Categoria)
 admin.site.register(Contato, ContatoAdmin)
--- contatos/views.py
+++ contatos/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 from .models import Contato
-
+from django.core.paginator import Paginator
 # Create your views here.
 
 def index(request):
@@ -15,10 +15,12 @@ def index(request):
         Contato.objects.all() é semelhante ao SELECT *FROM contatos do SQL
     """
     contato = Contato.objects.all()
+    paginas = Paginator(contato, 5)
+    page = request.GET.get('p')
+    contato = paginas.get_page(page)
     return render(request, 'contatos/index.html', {
         'cont': contato
     })
-
 
 def ver_contato(request, id_contato):
     """
@@ -26,8 +28,36 @@ def ver_contato(request, id_contato):
         
         Contato.objects.get(id = id_contato) pega apenas um dado, nesse caso o id da contato
     """
-    contato = Contato.objects.get(id = id_contato)
+#    contato = Contato.objects.get(id = id_contato)
+    contato = get_object_or_404(Contato, id = id_contato)
 
     return render(request, 'contatos/ver_contato.html',{
         'cont': contato
     })
+
+
+
+
+
+# uma das maneiras diferentes de tratar erros 
+
+'''
+def ver_contato(request, id_contato):
+    """
+        Metodo que pega um um contato da tabela contato pelo id 
+        
+        Contato.objects.get(id = id_contato) pega apenas um dado, nesse caso o id da contato
+    """
+    # tenta entrar na requisição da pagina de contatos, caso não encontra, mostra o erro 404
+    try:
+        contato = Contato.objects.get(id = id_contato)
+
+        return render(request, 'contatos/ver_contato.html',{
+            'cont': contato
+        })
+    except Contato.DoesNotExist as e:
+        raise Http404()
+
+
+'''
+
