16d4b7092f3f97d4dd86ba4df567b225c630dc89
Vinicius02612 <viniciusnunes02612@gmail.com>
2023-04-26 21:27:29 -0300

    preparando deploy

--- agenda/settings.py
+++ agenda/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i$@n__ii_m$p883$(g4$1ea955^5q2#0a96)vi+v0#w$x@0b%='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['localhost','13.58.154.141']
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
