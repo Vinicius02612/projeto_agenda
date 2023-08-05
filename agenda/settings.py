28fe23102d90a3b51ac9424c49ad5c98b4d2bc32
Vinicius02612 <viniciusnunes02612@gmail.com>
2023-08-04 21:24:48 -0300

    try deploy v5.0.

--- agenda/settings.py
+++ agenda/settings.py
@@ -139,9 +139,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     # and creating unique names for each version so they can safely be cached forever.
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-""" STATICFILES_DIRS = [
+STATICFILES_DIRS = [
     BASE_DIR /"templates/static",
-] """
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
